8504a59db17695acfa06b06cc20a38348445876d
sfmig <33267254+sfmig@users.noreply.github.com>
2024-12-12 12:59:58 +0000

    Add annotations_test_data fixture to its module

--- tests/fixtures/annotations.py
+++ tests/fixtures/annotations.py
@@ -1,1 +1,8 @@
-"""Pytest fixtures for annotation tests."""
+"""Pytest fixtures shared across annotation tests."""
+
+import pytest
+
+
+@pytest.fixture()
+def annotations_test_data(pooch_registry, get_paths_test_data):
+    return get_paths_test_data(pooch_registry, "test_annotations")
