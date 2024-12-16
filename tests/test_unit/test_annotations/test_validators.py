a6523e1650226734aa3b9b94c5ce9ec3cf9a488f
sfmig <33267254+sfmig@users.noreply.github.com>
2024-12-16 16:27:24 +0000

    Add tests for keys check WIP

--- tests/test_unit/test_annotations/test_validators.py
+++ tests/test_unit/test_annotations/test_validators.py
@@ -8,7 +8,10 @@ from ethology.annotations.json_schemas import (
     COCO_UNTRACKED_SCHEMA,
     VIA_UNTRACKED_SCHEMA,
 )
-from ethology.annotations.validators import ValidJSON
+from ethology.annotations.validators import (
+    ValidJSON,
+    ValidVIAUntrackedJSON,
+)
 
 
 @pytest.fixture()
@@ -73,6 +76,47 @@ def _json_file_with_schema_error(out_parent_path, json_valid_path):
     return out_json
 
 
+@pytest.fixture()
+def via_json_file_with_missing_keys(tmp_path, annotations_test_data):
+    def _via_json_file_with_missing_keys(
+        valid_json_filename, required_keys_to_pop
+    ):
+        """Return path to a JSON file that is missing required keys."""
+        # read valid json file
+        valid_json_path = annotations_test_data[valid_json_filename]
+        with open(valid_json_path) as f:
+            data = json.load(f)
+
+        # remove any keys in the first level
+        for key in required_keys_to_pop.get("main", []):
+            data.pop(key)
+
+        # remove keys in nested dicts
+        for _, img_dict in data["_via_img_metadata"].items():
+            # remove keys for each image dictionary
+            for key in required_keys_to_pop.get("image_keys", []):
+                img_dict.pop(key)
+
+            for region in img_dict["regions"]:
+                # remove keys for each region
+                for key in required_keys_to_pop.get("region_keys", []):
+                    region.pop(key)
+
+                # remove keys under shape_attributes
+                for key in required_keys_to_pop.get(
+                    "shape_attributes_keys", []
+                ):
+                    region["shape_attributes"].pop(key)
+
+        # save the modified json to a new file
+        out_json = tmp_path / f"{valid_json_path.name}_missing_keys.json"
+        with open(out_json, "w") as f:
+            json.dump(data, f)
+        return out_json
+
+    return _via_json_file_with_missing_keys
+
+
 @pytest.mark.parametrize(
     "input_file_standard, input_schema",
     [
@@ -135,7 +179,7 @@ def test_valid_json(
         ),
     ],
 )
-def test_valid_json_error(
+def test_valid_json_errors(
     invalid_json_file_str,
     input_schema,
     expected_exception,
@@ -152,3 +196,80 @@ def test_valid_json_error(
         assert log_message in str(excinfo.value)
     else:
         assert log_message.format(invalid_json_file) == str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    "input_json_file",
+    [
+        "VIA_JSON_sample_1.json",
+        "VIA_JSON_sample_2.json",
+    ],
+)
+def test_valid_via_untracked_json(annotations_test_data, input_json_file):
+    filepath = annotations_test_data[input_json_file]
+    with does_not_raise():
+        ValidVIAUntrackedJSON(
+            path=filepath,
+        )
+
+
+@pytest.mark.parametrize(
+    "valid_json_file",
+    [
+        "VIA_JSON_sample_1.json",
+        "VIA_JSON_sample_2.json",
+    ],
+)
+@pytest.mark.parametrize(
+    "missing_keys, expected_exception, log_message",
+    [
+        (
+            {"main": ["_via_image_id_list"]},
+            pytest.raises(ValueError),
+            "Required key(s) {'_via_image_id_list'} not found "
+            "in ['_via_settings', '_via_img_metadata', '_via_attributes', "
+            "'_via_data_format_version'].",
+        ),
+        (
+            {"image_keys": ["filename"]},
+            pytest.raises(ValueError),
+            "Required key(s) {'filename'} not found "
+            "in ['size', 'regions', 'file_attributes'] "
+            "for 09.08_09.08.2023-01-Left_frame_001764.png15086122.",
+        ),
+        (
+            {"region_keys": ["shape_attributes"]},
+            pytest.raises(ValueError),
+            "The JSON data does not contain the required keys: annotations.",
+        ),
+        (
+            {"shape_attributes_keys": ["x"]},
+            pytest.raises(ValueError),
+            "The JSON data does not contain the required keys: annotations.",
+        ),
+    ],
+)
+def test_valid_via_untracked_json_missing_keys(
+    valid_json_file,
+    missing_keys,
+    via_json_file_with_missing_keys,
+    expected_exception,
+    log_message,
+):
+    # create invalid json file with missing keys
+    invalid_json_file = via_json_file_with_missing_keys(
+        valid_json_file, missing_keys
+    )
+
+    # run validatio
+    with expected_exception as excinfo:
+        ValidVIAUntrackedJSON(
+            path=invalid_json_file,
+        )
+
+    assert str(excinfo.value) == log_message
+
+
+# def test_valid_via_untracked_json ---> checks required keys
+# def test_valid_coco_untracked_json ---> checks required keys
+# def test_check_keys?
