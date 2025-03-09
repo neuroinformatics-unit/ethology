904d624e77188f1abfda8b76c2ac142156d4d417
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2025-03-09 22:06:00 +0000

    add error handling

--- ethology/io/compress.py
+++ ethology/io/compress.py
@@ -1,22 +1,37 @@
-import typing
+import shutil
 
 import ffmpeg
-from movement import sample_data
 
+
+class FFmpegNotFoundError(Exception):
+    """Custom exception for missing ffmpeg installation."""
+    pass
+
+def check_ffmpeg_installed():
+    """Check if ffmpeg is installed by looking for the executable."""
+    if shutil.which("ffmpeg") is None:
+        raise FFmpegNotFoundError("FFmpeg is not installed or not found in system PATH.")
+    else:
+        return True
 
 def compress_video_h264(video_path:str, 
                   saveout_path:str, 
                   crf: int = 23,
+                  overwrite_output:bool = False
 ) -> None:  
-    """ Compresses and optionally trims the local video, and saves out. 
+    """ Compresses a local video, and saves out. 
 
     Args:
         video_path (str): local video filepath to be trimmed
         saveout_path (str): local filepath for saving file to
         crf (int, optional): Compression rate factor. Defaults to 23.
+        overwrite_output (bool, optional): Whether to overwrite an existing video on saveout_path.
     """
-    input = ffmpeg.input(video_path)
-    output = input.output(saveout_path, vcodec='libx264', pix_fmt='yuv420p', preset='superfast', crf=crf)
-    output.run(overwrite_output = True)
 
-
+    # check_ffmpeg_installed()
+    try: 
+        input = ffmpeg.input(video_path)
+        output = input.output(saveout_path, vcodec='libx264', pix_fmt='yuv420p', preset='superfast', crf=crf)
+        output.run(overwrite_output = overwrite_output, capture_stdout=True, capture_stderr=True)
+    except ffmpeg._run.Error as e:
+        raise RuntimeError(f"FFmpeg Error: \n {e.stderr.decode() if e.stderr else "No stderr"}")
