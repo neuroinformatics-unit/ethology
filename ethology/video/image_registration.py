11d67c540c37a83512d6a2d462e1f288ec11b982
Mahima Pattanayak <164752721+Mahima-Pattanayak@users.noreply.github.com>
2025-03-23 17:24:04 +0530

    Update image_registration.py
    Signed-off-by: Mahima Pattanayak  <164752721+Mahima-Pattanayak@users.noreply.github.com>

--- ethology/video/image_registration.py
+++ ethology/video/image_registration.py
@@ -1,15 +1,20 @@
+"""
+This module provides image registration functionality using Elastix (SimpleITK).
+"""
 import SimpleITK as sitk
 
 
 def register_images(fixed_image_path, moving_image_path, output_path):
-    """Register a moving image to a fixed image using Elastix.
+    """
+    Register a moving image to a fixed image using Elastix.
 
-    This function computes a rigid transformation to align the moving image with the fixed image.
+    This function computes a rigid transformation to align 
+    the moving image with the fixed image.
 
     :param fixed_image_path: Path to the reference image.
     :param moving_image_path: Path to the image to be aligned.
     :param output_path: Path to save the registered image.
-    """
+    """ 
     # Read images using SimpleITK
     fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
     moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
