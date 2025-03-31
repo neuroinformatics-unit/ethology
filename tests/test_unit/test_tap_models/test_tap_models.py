040e2d8e26bf7b8bb9c0bf3e78f2db12dfc78d96
parikshit14 <14.parikshitsingh@gmail.com>
2025-03-31 11:32:19 +0530

    test convertion to movement dataset

--- tests/test_unit/test_tap_models/test_tap_models.py
+++ tests/test_unit/test_tap_models/test_tap_models.py
@@ -41,6 +41,35 @@ def test_track_with_invalid_video_path(video_path, exception, match):
         tracker.track(video_path=video_path, query_points=[[0, 0, 0]])
 
 
+def test_convert_to_movement_dataset():
+    # check before conversion to movement and after the data is same.
+    n_frames = 50
+    n_individuals = 4
+
+    pred_tracks = torch.randint(0, 500, (1, n_frames, n_individuals, 2))
+    # pred_visibility = torch.randint(0, 2, (1, 50, 4, 2), dtype=torch.bool)
+
+    tracker = BaseTrackAnyPoint(model="cotracker")
+    ds = tracker.convert_to_movement_dataset(pred_tracks)
+    x_positions = ds.position.sel(
+        space="x"
+    ).values  # Shape: (time, keypoints, individuals)
+    y_positions = ds.position.sel(space="y").values
+
+    for ind in range(n_individuals):
+        print(f"Individual: {ind}")
+        x_coords = np.array(x_positions[:, :, ind]).flatten().tolist()
+        y_coords = np.array(y_positions[:, :, ind]).flatten().tolist()
+
+        # comparing size of x and y coordinates
+        assert len(x_coords) == len(pred_tracks[0, :, ind, 0])
+        assert len(y_coords) == len(pred_tracks[0, :, ind, 1])
+
+        # comparing individual elements
+        assert x_coords == pred_tracks[0, :, ind, 0].tolist()
+        assert y_coords == pred_tracks[0, :, ind, 1].tolist()
+
+
 def test_track_with_valid_parameters():
     n_frames = 50
     n_keypoints = 1
@@ -66,6 +95,8 @@ def test_track_with_valid_parameters():
         ds = tracker.track(
             video_path="fake_video_path.mp4", query_points=query_points
         )
+
+        # checking individual dimensions
         assert ds.position.shape == (n_frames, 2, n_keypoints, n_individuals)
         assert ds.source_software == "cotracker"
         assert len(ds.individuals) == n_individuals
