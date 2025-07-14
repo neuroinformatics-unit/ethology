b9b5a99e5f08015eb7fcf7ac26d38a9abb67b138
sfmig <33267254+sfmig@users.noreply.github.com>
2025-07-14 13:36:32 +0100

    Return detections from pytorch dataset as xarray ds

--- ethology/detectors/inference.py
+++ ethology/detectors/inference.py
@@ -2,6 +2,116 @@
 
 import numpy as np
 import torch
+import xarray as xr
+
+
+def _pad_sequence_along_detections_dim(
+    array: np.ndarray, max_n_detections_per_image: int
+) -> tuple:
+    """Return sequence for padding input array along detections dimension."""
+    pad_detections_per_image = max_n_detections_per_image - array.shape[0]
+    return tuple(
+        (0, pad_detections_per_image) if i == 0 else (0, 0)
+        for i in range(array.ndim)
+    )
+
+
+def _detections_per_sample_as_ds(
+    detections_per_sample: dict,
+) -> xr.Dataset:
+    """Reshape detections per sample as xarray dataset."""
+    # Get coordinates
+    list_image_id_coords = list(detections_per_sample.keys())
+    list_space_coords = ["x", "y"]
+    max_n_detections_per_image = max(
+        [
+            detections["boxes"].shape[0]
+            for detections in detections_per_sample.values()
+        ]
+    )
+
+    list_id_coords = list(range(max_n_detections_per_image))  # per frame
+    coords_dict = {
+        "image_id": list_image_id_coords,
+        "space": list_space_coords,
+        "id": list_id_coords,  # per frame
+    }
+    coords_dict_no_space = coords_dict.copy()
+    del coords_dict_no_space["space"]
+
+    # Get lists of data arrays
+    list_centroid_arrays = [
+        (
+            detections["boxes"].cpu().numpy()[:, 0:2]
+            + detections["boxes"].cpu().numpy()[:, 2:4]
+        )
+        * 0.5
+        for detections in detections_per_sample.values()
+    ]
+
+    list_shape_arrays = [
+        detections["boxes"].cpu().numpy()[:, 2:4]
+        - detections["boxes"].cpu().numpy()[:, 0:2]
+        for detections in detections_per_sample.values()
+    ]
+
+    list_confidence_arrays = [
+        detections["scores"].cpu().numpy()  # .reshape(-1, 1)
+        for detections in detections_per_sample.values()
+    ]
+
+    list_label_arrays = [
+        detections["labels"].cpu().numpy()  # .reshape(-1, 1)
+        for detections in detections_per_sample.values()
+    ]
+
+    # Define arrays to create
+    arrays_dict = {
+        "centroids": {
+            "data": list_centroid_arrays,
+            "coords": coords_dict,
+            "pad_value": np.nan,
+        },
+        "shape": {
+            "data": list_shape_arrays,
+            "coords": coords_dict,
+            "pad_value": np.nan,
+        },
+        "confidence": {
+            "data": list_confidence_arrays,
+            "coords": coords_dict_no_space,
+            "pad_value": np.nan,
+        },
+        "label": {
+            "data": list_label_arrays,
+            "coords": coords_dict_no_space,
+            "pad_value": -1,
+        },
+    }
+
+    # Create all DataArrays in a loop
+    data_arrays = {}
+    for name in arrays_dict:
+        data_arrays[name] = xr.DataArray(
+            data=np.stack(
+                [
+                    np.pad(
+                        array,
+                        _pad_sequence_along_detections_dim(
+                            array, max_n_detections_per_image
+                        ),
+                        mode="constant",
+                        constant_values=arrays_dict[name]["pad_value"],
+                    ).T
+                    for array in arrays_dict[name]["data"]
+                ],
+                axis=0,  # need to pad with nans for constant shape
+            ),
+            dims=list(arrays_dict[name]["coords"].keys()),
+            coords=arrays_dict[name]["coords"],
+        )
+
+    return xr.Dataset(data_vars=data_arrays)
 
 
 def run_detector_on_dataset(
@@ -23,7 +133,7 @@ def run_detector_on_dataset(
 
     # Run detection
     detections_per_sample = {}
-    for idx, (image, annotations) in enumerate(dataset):
+    for idx, (image, _annotations) in enumerate(dataset):
         # Place image tensor on device and add batch dimension
         image = image.to(device)[None]  # [1, C, H, W]
 
@@ -34,7 +144,17 @@ def run_detector_on_dataset(
         # Add to dict
         detections_per_sample[idx] = detections
 
-    return detections_per_sample
+    # Format as xarray dataset
+    detections_dataset = _detections_per_sample_as_ds(detections_per_sample)
+
+    return detections_dataset
+
+
+def _detections_per_batch_as_ds(
+    detections_per_batch: dict,
+) -> xr.Dataset:
+    """Reshape detections per batch as xarray dataset."""
+    pass
 
 
 def run_detector_on_dataloader(
@@ -44,7 +164,7 @@ def run_detector_on_dataloader(
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Run detection on a dataloader.
 
-    The output is a list of dictionary with the detections per batch.
+    The output is a dictionary with the detections per batch as a list.
     The detections dictionary has the following keys:
     - "boxes": tensor of shape [N, 4]
     - "scores": tensor of shape [N]
@@ -57,7 +177,7 @@ def run_detector_on_dataloader(
     detections_per_batch = {}
     for batch_idx, (image_batch, _annotations_batch) in enumerate(dataloader):
         # Place batch of images on device
-        image_batch = image_batch.to(device)  # [B, C, H, W]
+        image_batch = [img.to(device) for img in image_batch]  # [B, C, H, W]
 
         # Run detection
         with torch.no_grad():
@@ -69,6 +189,30 @@ def run_detector_on_dataloader(
         detections_per_batch[batch_idx] = detections_batch
 
     return detections_per_batch
+
+
+def collate_fn_varying_n_bboxes(batch: tuple) -> tuple:
+    """Collate function for dataloader with varying number of bounding boxes.
+
+    A custom function is needed for detection
+    because the number of bounding boxes varies
+    between images of the same batch.
+    See https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#data-loading-and-training-loop
+
+    Parameters
+    ----------
+    batch : tuple
+        a tuple of 2 tuples, the first one holding all images in the batch,
+        and the second one holding the corresponding annotations.
+
+    Returns
+    -------
+    tuple
+        a tuple of length = batch size, made up of (image, annotations)
+        tuples.
+
+    """
+    return tuple(zip(*batch, strict=False))
 
 
 # def run_detector_on_image(
