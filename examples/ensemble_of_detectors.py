099f58651191c2903b020d29a3182c4be94f23f8
sfmig <33267254+sfmig@users.noreply.github.com>
2025-11-13 21:27:43 +0000

    Add evalution

--- examples/ensemble_of_detectors.py
+++ examples/ensemble_of_detectors.py
@@ -11,11 +11,11 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
 
 from ethology.detectors.ensembles.models import EnsembleDetector
-
-# from ethology.detectors.evaluate import compute_precision_recall_ds
-# from ethology.io.annotations import load_bboxes
+from ethology.detectors.evaluate import compute_precision_recall_ds
+from ethology.io.annotations import load_bboxes
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 # Helper functions
 def create_coco_dataset(
@@ -106,6 +106,11 @@ dataloader = DataLoader(
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Define a YAML config file for the ensemble of trained detectors
+experiment_ID = "617393114420881798"
+ml_runs_experiment_dir = (
+    Path("/home/sminano/swc/project_crabs/ml-runs") / experiment_ID
+)
+last_ckpt = Path("checkpoints") / "last.ckpt"
 
 config = {
     "models": {
@@ -117,15 +122,43 @@ config = {
             "weights_backbone": None,
         },
         "checkpoints": [
-            "/home/sminano/swc/project_crabs/ml-runs/617393114420881798/f348d9d196934073bece1b877cbc4d38/checkpoints/last.ckpt",
-            "/home/sminano/swc/project_crabs/ml-runs/617393114420881798/879d2f77e2b24adcb06b87d2fede6a04/checkpoints/last.ckpt",
+            str(
+                ml_runs_experiment_dir
+                / "f348d9d196934073bece1b877cbc4d38"
+                / last_ckpt
+            ),  # above_0th
+            str(
+                ml_runs_experiment_dir
+                / "879d2f77e2b24adcb06b87d2fede6a04"
+                / last_ckpt
+            ),  # above_1st
+            str(
+                ml_runs_experiment_dir
+                / "75583ec227e3444ab692b99c64795325"
+                / last_ckpt
+            ),  # above_5th
+            str(
+                ml_runs_experiment_dir
+                / "4acc37206b1e4f679d535c837bee2c2f"
+                / last_ckpt
+            ),  # above_10th
+            str(
+                ml_runs_experiment_dir
+                / "fdcf88fcbcc84fbeb94b45ca6b6f8914"
+                / last_ckpt
+            ),  # above_25th
+            str(
+                ml_runs_experiment_dir
+                / "daa05ded0ea047388c9134bf044061c5"
+                / last_ckpt
+            ),  # above_50th
         ],
     },
     "fusion": {
         "method": "wbf",
         "iou_th_ensemble": 0.5,
         "skip_box_th": 0.0001,
-        "n_jobs": 2,  # workers for joblib.Parallel, n_workers should be <= number of CPU cores
+        "n_jobs": -1,  # workers for joblib.Parallel, n_workers should be <= number of CPU cores
         # "confidence_threshold_post_fusion": 0.0,
         # "max_n_detections": 300
     },
@@ -150,23 +183,46 @@ fused_detections_ds = ensemble_detector.format_predictions(raw_predictions)
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# # Evaluate the ensemble model
-# # - load ground truth
-# # - compute metrics
+# Remove low confidence detections
+confidence_threshold_post_fusion = 0.5
+fused_detections_ds_ = fused_detections_ds.where(
+    fused_detections_ds.confidence >= confidence_threshold_post_fusion
+)
 
-# gt_bboxes_ds = load_bboxes.from_files(annotations_file_path, format="COCO")
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Evaluate the ensemble model
+# - load ground truth
+# - compute metrics
 
+gt_bboxes_ds = load_bboxes.from_files(annotations_file_path, format="COCO")
 
-# fused_detections_ds, gt_bboxes_ds = compute_precision_recall_ds(
-#     pred_bboxes_ds=fused_detections_ds,
-#     gt_bboxes_ds=gt_bboxes_ds,
-#     iou_threshold=0.1,  # change to 0.5?
-# )
+iou_threshold_tp = 0.25
+fused_detections_ds, gt_bboxes_ds = compute_precision_recall_ds(
+    pred_bboxes_ds=fused_detections_ds_,
+    gt_bboxes_ds=gt_bboxes_ds,
+    iou_threshold=iou_threshold_tp,
+)
 
+# All models on full August dataset, without removing low confidence detections:
+# confidence_threshold_post_fusion = 0.0
+# Precision: 0.5920
+# Recall: 0.8455
+# ---
+# confidence_threshold_post_fusion = 0.4
+# Precision: 0.8339
+# Recall: 0.7177
+# ---
+# confidence_threshold_post_fusion = 0.5
+# Precision: 0.8714
+# Recall: 0.6624
+# ---
 
-# print(
-#     "Ensemble model with confidence threshold post fusion: "
-#     f"{ensemble_detector.config['fusion']['confidence_threshold_post_fusion']}"
-# )
-# print(f"Precision: {fused_detections_ds.precision.mean().values:.4f}")
-# print(f"Recall: {fused_detections_ds.recall.mean().values:.4f}")
+print(
+    "Ensemble model with confidence threshold post fusion: "
+    f"{confidence_threshold_post_fusion:.2f}"
+)
+print(f"Precision: {fused_detections_ds.precision.mean().values:.4f}")
+print(f"Recall: {fused_detections_ds.recall.mean().values:.4f}")
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Plot calibration curve
