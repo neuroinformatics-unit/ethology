39112a03a03a4d49cd0a2ddd15f10f5385c321d4
sfmig <33267254+sfmig@users.noreply.github.com>
2025-12-12 17:48:16 +0000

    Expand to other torchvision detectors (WIP)

--- ethology/detectors/models.py
+++ ethology/detectors/models.py
@@ -9,10 +9,7 @@ import xarray as xr
 from lightning import LightningModule, Trainer
 from torch.utils.data import DataLoader
 from torchvision.models import get_model
-from torchvision.models.detection import (
-    faster_rcnn,
-    fasterrcnn_resnet50_fpn_v2,
-)
+from torchvision.models.detection import faster_rcnn, fcos, retinanet
 
 from ethology.detectors.utils import (
     _corners_to_centroid_shape,
@@ -20,6 +17,16 @@ from ethology.detectors.utils import (
 )
 from ethology.validators.detections import ValidBboxDetectionsDataset
 from ethology.validators.utils import _check_output
+
+# Registry of supported models with their constructors
+MODEL_REGISTRY = {
+    "fasterrcnn_resnet50_fpn_v2": (faster_rcnn.fasterrcnn_resnet50_fpn_v2),
+    "fasterrcnn_mobilenet_v3_large_fpn": (
+        faster_rcnn.fasterrcnn_mobilenet_v3_large_fpn
+    ),
+    "fcos_resnet50_fpn": fcos.fcos_resnet50_fpn,
+    "retinanet_resnet50_fpn_v2": retinanet.retinanet_resnet50_fpn_v2,
+}
 
 
 class SingleDetector(LightningModule):
@@ -58,18 +65,63 @@ class SingleDetector(LightningModule):
         Use default weights, backbone, and box predictor. Initialise
         classification head with random weights and size to the number
         of classes.
-        """
-        # TODO: expand to other torchvision detectors?
-        # Load Faster-RNN with pretreained weights in backbone and head
-        model = fasterrcnn_resnet50_fpn_v2(weights="DEFAULT")
 
-        # Replace final classification layer only
-        # (while keeping bbox regression weights)
-        in_features = model.roi_heads.box_predictor.cls_score.in_features
-        model.roi_heads.box_predictor = faster_rcnn.FastRCNNPredictor(
-            in_features,
-            self.config["num_classes"],
+        Keeping the classification head may not be useful if the domain
+        is very different to COCO or natural images, or if looking for
+        fine grained detection. They may be helpful in small datasets (< 100
+        images)
+        """
+        # Get model name and number of classes
+        # Default: fasterrcnn_resnet50_fpn_v2 and 2 classes
+        model_name = self.config.get(
+            "model_name", "fasterrcnn_resnet50_fpn_v2"
         )
+        num_classes = self.config.get("num_classes", 2)
+
+        # Check if model is supported
+        if model_name not in MODEL_REGISTRY:
+            raise ValueError(
+                f"Model '{model_name}' not supported. "
+                f"Available: {list(MODEL_REGISTRY.keys())}"
+            )
+
+        # Load selected model with pretreained weights in backbone and head
+        # For faster-rcnn: both backbone and RPN are loaded with pretrained
+        # weights
+        model = MODEL_REGISTRY[model_name](weights="DEFAULT")
+
+        # Keep as much as possible from the bbox prediction head
+        if "fasterrcnn" in model_name:
+            # Reinitialise box predictor for the required number of classes
+            # (both cls_score and bbox_pred are reinitialised)
+            # Note: in Faster R-CNN, the bbox regression is class-specific;
+            # it learns a different way of refining bboxes for each class.
+            # So we need to reinitialise the full box predictor.
+            in_features = model.roi_heads.box_predictor.cls_score.in_features
+            model.roi_heads.box_predictor = faster_rcnn.FastRCNNPredictor(
+                in_features,
+                self.config["num_classes"],
+            )
+        elif "retinanet" in model_name:
+            # CHECK
+            # In retinanet bbox regression is class-agnostic
+            in_channels = model.head.classification_head.conv[0].in_channels
+            num_anchors = model.head.classification_head.num_anchors
+            model.head.classification_head = (
+                retinanet.RetinaNetClassificationHead(
+                    in_channels, num_anchors, num_classes
+                )
+            )
+
+        elif "fcos" in model_name:
+            # CHECK
+            # In fcos bbox regression is class-agnostic
+            in_channels = model.head.classification_head.conv[0].in_channels
+            num_anchors = model.head.classification_head.num_anchors
+            model.head.classification_head = fcos.FCOSClassificationHead(
+                in_channels, num_anchors, num_classes
+            )
+
         return model
 
     def _configure_model_from_checkpoint(
