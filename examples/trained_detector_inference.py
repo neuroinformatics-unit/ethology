23e4dc9c7855e82db80f9a4cfd209eafa9ab7c66
sfmig <33267254+sfmig@users.noreply.github.com>
2025-12-23 15:35:06 +0000

    Verify proto example runs with latest implementation

--- examples/trained_detector_inference.py
+++ examples/trained_detector_inference.py
@@ -16,6 +16,7 @@ Run inference with a trained detector on a dataset of images for proofreading.
 # Imports
 # -------
 import os
+from datetime import datetime
 from pathlib import Path
 
 from lightning import Trainer
@@ -26,7 +27,7 @@ from ethology.datasets.inference import (
     get_default_inference_transforms,
 )
 from ethology.detectors.models import ObjectDetector
-from ethology.io.annotations import save_bboxes
+from ethology.io.annotations import load_bboxes, save_bboxes
 
 # For interactive plots: install ipympl with `pip install ipympl` and uncomment
 # the following line in your notebook
@@ -64,7 +65,7 @@ config = {
     "checkpoint": (
         "/home/sminano/swc/project_crabs/ml-runs/"
         "617393114420881798/f348d9d196934073bece1b877cbc4d38/checkpoints/last.ckpt"
-    ),
+    ),  # all annotations
 }
 
 # if no classes are specified: 91 (coco2017)
@@ -155,17 +156,28 @@ predictions_ds = model.run_inference(trainer, dataloader, attrs=ds_attrs)
 # %%
 # Export predictions as COCO annotations for proofreading
 # ---------------------------------------------------------
-
-out_file = save_bboxes.to_COCO_file(predictions_ds, output_filepath="out.json")
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+out_file = save_bboxes.to_COCO_file(
+    predictions_ds, output_filepath=f"out_{timestamp}.json"
+)
 
 # %%
 # Load proofread annotations and compare
 # ---------------------------------------
 
-# proofread_ds = load_bboxes.from_files(
-#     "via_project_11Dec2025_12h44m_coco.json", format="COCO"
-# )
-#
+proofread_ds = load_bboxes.from_files(
+    "/home/sminano/swc/project_ethology/ethology/via_project_23Dec2025_15h25m_coco.json",
+    format="COCO",
+)
+
+# I added a box in image_id=4:
+# confirm filename:
+# predictions_ds.map_image_id_to_filename[4]
+# --> 'Sep2023_day4_reencoded/07.09.2023-01-Left_RE_frame_046095.png'
+# predictions_ds.confidence.sel(image_id=4).notnull().sum()
+# --> 74 predictions
+# proofread_ds.position.sel(image_id=4,space='x').notnull().sum()
+# --> 75 annotations
 
 # %%
 # Clean-up
