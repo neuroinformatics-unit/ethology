a7ce3d2190a174f60fa81e1aaccc8d52e613afba
sfmig <33267254+sfmig@users.noreply.github.com>
2025-12-23 13:37:53 +0000

    Add types for clarity in utils. Edit docstring

--- ethology/detectors/utils.py
+++ ethology/detectors/utils.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def _get_padding_width(array, final_first_dim):
+def _get_padding_width(array: np.ndarray, final_first_dim: int) -> list[tuple]:
     """Get pad_width to pad the end of an array along the first dimension."""
     # Throw an error if shape mismatch
     if array.shape[0] > final_first_dim:
@@ -16,7 +16,9 @@ def _get_padding_width(array, final_first_dim):
     return pad_width
 
 
-def _pad_to_max_first_dimension(list_arrays, fill_value=np.nan):
+def _pad_to_max_first_dimension(
+    list_arrays: list[np.ndarray], fill_value=np.nan
+) -> list[np.ndarray]:
     """Pad arrays in list to maximum size of their first dimension."""
     max_first_dimension = max(array.shape[0] for array in list_arrays)
 
@@ -45,35 +47,45 @@ def _pad_to_max_first_dimension(list_arrays, fill_value=np.nan):
     return list_arrays_padded
 
 
-def centroid_shape_to_corners(position: np.ndarray, shape: np.ndarray):
-    """Convert centroid and shape arrays to x1y1, x2y2 corner arrays.
+def centroid_shape_to_corners(
+    centroid: np.ndarray, shape: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
+    """Convert box centroid and shape arrays to x1y1, x2y2 corner arrays.
+
+    The function assumes all coordinates are expressed in an image coordinate
+    system whose origin is at the centre of the top-left pixel in the image,
+    its x coordinate values increase from left to right of the image,
+    and its y coordinate values increase from top to bottom.
 
     Parameters
     ----------
-    position : numpy.ndarray
-        Array of centroid coordinates with shape (..., 2, ...), where the
-        second dimension contains (x, y) coordinates.
-    shape : numpy.ndarray
+    centroid
+        Array of bounding box centroid coordinates with shape (..., 2, ...),
+        where the second dimension contains (x, y) coordinates in the image
+        coordinate system.
+    shape
         Array of bounding box dimensions with shape (..., 2, ...), where the
-        second dimensioncontains (width, height).
+        second dimension contains (width, height) values, in the same units as
+        the ``centroid`` array.
 
     Returns
     -------
     x1y1 : numpy.ndarray
-        Array of top-left corner coordinates with shape (..., 2,..), where the
-        second dimension contains (x, y) coordinates. The top-left corner
-        is the bounding box corner with minimum x and y coordinate values.
+        Array of bounding box top-left corner coordinates
+        with shape (..., 2,..), where the second dimension contains (x, y)
+        coordinates. The top-left corner is the corner of the bounding box
+        with minimum x and y coordinates in the image coordinate system.
     x2y2 : numpy.ndarray
         Array of bottom-right corner coordinates with shape (..., 2,..), where
         the second dimension contains (x, y) coordinates. The bottom-right
-        corner is the bounding box corner with maximum x and y coordinate
-        values.
-
+        corner is the corner of the bounding box with maximum x and y
+        coordinates in the image coordinate system.
 
     Raises
     ------
-    ValueError : If position and shape have different shapes or
-    second dimension is not 2.
+    ValueError
+        If ``position`` and ``shape`` arrays have different shapes, or
+        if any of their second dimensions is not 2.
 
     See Also
     --------
@@ -81,56 +93,67 @@ def centroid_shape_to_corners(position: np.ndarray, shape: np.ndarray):
 
     """
     # Check position and shape have compatible shapes
-    if position.shape != shape.shape:
+    if centroid.shape != shape.shape:
         raise ValueError(
             f"position and shape must have the same shape, "
-            f"got {position.shape} and {shape.shape}"
+            f"got {centroid.shape} and {shape.shape}"
         )
 
-    # Check dimension at index 1 is 2D
-    if position.shape[1] != 2 or shape.shape[1] != 2:
+    # Check size of second dimension is 2D
+    if centroid.shape[1] != 2 or shape.shape[1] != 2:
         raise ValueError(
-            "Dimension at index 1 must be 2"
-            " for both position and shape, "
-            f"but got position: {position.shape}, shape: {shape.shape}"
+            "Dimension at index 1 must be 2 "
+            "for both position and shape arrays, "
+            f"but got position: {centroid.shape}, shape: {shape.shape}"
         )
 
     half_shape = shape / 2
     return (
-        position - half_shape,  # x1y1
-        position + half_shape,  # x2y2
+        centroid - half_shape,  # x1y1, top-left corner
+        centroid + half_shape,  # x2y2, bottom-right corner
     )
 
 
-def corners_to_centroid_shape(x1y1: np.ndarray, x2y2: np.ndarray):
-    """Convert x1y1, x2y2 corner arrays to centroid and shape arrays.
+def corners_to_centroid_shape(
+    x1y1: np.ndarray, x2y2: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
+    """Convert x1y1, x2y2 box corner arrays to centroid and shape arrays.
+
+    The function assumes all coordinates are expressed in an image coordinate
+    system whose origin is at the centre of the top-left pixel in the image,
+    its x coordinate values increase from left to right of the image,
+    and its y coordinate values increase from top to bottom.
 
     Parameters
     ----------
-    x1y1 : numpy.ndarray
-        Array of top-left corner coordinates with shape (..., 2, ...), where
-        the second dimension contains (x, y) coordinates. The top-left
-        corner is the bounding box corner with minimum x and y coordinate
-        values.
-    x2y2 : numpy.ndarray
-        Array of bottom-right corner coordinates with shape (..., 2, ...),
-        where the second dimension contains (x, y) coordinates.
-        The bottom-right corner is the bounding box corner with maximum x
-        and y coordinate values.
+    x1y1
+        Array of bounding box top-left corner coordinates
+        with shape (..., 2,..), where the second dimension contains (x, y)
+        coordinates. The top-left corner is the corner of the bounding box
+        with minimum x and y coordinates in the image coordinate system.
+    x2y2
+        Array of bottom-right corner coordinates with shape (..., 2,..), where
+        the second dimension contains (x, y) coordinates. The bottom-right
+        corner is the corner of the bounding box with maximum x and y
+        coordinates in the image coordinate system.
 
     Returns
     -------
     centroid : numpy.ndarray
-        Array of centroid coordinates with shape (..., 2, ...), where the
-        second dimension contains (x, y) coordinates
+        Array of bounding box centroid coordinates with shape (..., 2, ...),
+        where the second dimension contains (x, y) coordinates in the image
+        coordinate system.
+
     shape : numpy.ndarray
-        Array of bounding box dimensions with shape (..., 2, ... ), where the
-        second dimension contains (width, height).
+        Array of bounding box dimensions with shape (..., 2, ...), where the
+        second dimension contains (width, height) values, in the same units as
+        the ``centroid`` array.
 
     Raises
     ------
-    ValueError : If x1y1 and x2y2 have different shapes or dimension
-    at index 1 is not 2.
+    ValueError
+        If x1y1 and x2y2 have different shapes, or
+        if any of their second dimensions is not 2.
 
     See Also
     --------
