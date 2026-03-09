cd102fa6f346556e7c01936db17e3beffab5f18e
Swagat Mishra <mishraswagat2804@gmail.com>
2026-03-10 00:08:48 +0530

    Add sample_data module using pooch for example datasets

---
+++ ethology/datasets/sample_data.py
@@ -0,0 +1,20 @@
+import pooch
+
+DATA_REGISTRY = pooch.create(
+    path=pooch.os_cache("ethology"),
+    base_url="https://example-dataset-url/",
+    registry={
+        "example_tracking.csv": None,
+    },
+)
+
+def load_sample_tracking():
+    """
+    Fetch a sample tracking dataset.
+
+    Returns
+    -------
+    str
+        Path to downloaded dataset.
+    """
+    return DATA_REGISTRY.fetch("example_tracking.csv")
