52064872158adbc73e88336750a7359852113360
Varun Kumar M <32513756+MKVarun@users.noreply.github.com>
2026-03-31 00:26:16 +0530

    Add utility function for video compression

--- ethology/io/video_utils.py
+++ ethology/io/video_utils.py
@@ -90,3 +90,92 @@ def get_video_specs(video_path: str):
         "duration": float(data.get("format", {}).get("duration", 0)),
         "streams": streams,
     }
+
+
+def compress_video(
+    input_path: str,
+    output_path: str,
+    crf: int = 23,
+    preset: str = "superfast",
+    overwrite: bool = True,
+):
+    """Compress video using H.264 codec with specified quality settings.
+
+    Parameters
+    ----------
+    input_path : str
+            Path to the input video file.
+
+    output_path : str
+            Path where the compressed video file will be saved.
+
+    crf : int, optional
+            Constant Rate Factor determining the quality and bitrate.
+            Lower values yield higher quality and larger file sizes
+            (range 0-51, typical 18-28).
+            Default is 23.
+
+    preset : str, optional
+            The encoding speed preset. Faster presets result in larger files
+            but quicker encoding. Options include 'ultrafast', 'superfast',
+            'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower',
+            'veryslow'.
+            Default is 'superfast'.
+
+    overwrite : bool, optional
+            If True, overwrite the output file if it already exists.
+            Default is True.
+
+    Returns
+    -------
+    bool
+            True if successful, False if an error occurred
+
+    Raises
+    ------
+            FileNotFoundError if the video file does not exist.
+
+    Example:
+    --------
+    >>> from ethology.io.video_utils import compress_video
+    >>> compress_video("input.mp4", "output.mp4")
+           True
+    >>> compress_video("input.mp4", "output.mp4", crf=20, preset="medium")
+           True
+
+    """
+    path = Path(input_path)
+    if not path.exists():
+        raise FileNotFoundError(f"Video file not found: {input_path}")
+
+    cmd = [
+        "ffmpeg",
+        "-y" if overwrite else "",
+        "-i",
+        str(input_path),
+        "-c:v",
+        "libx264",
+        "-pix_fmt",
+        "yuv420p",
+        "-preset",
+        preset,
+        "-crf",
+        str(crf),
+        "-progress",
+        "pipe:1",
+        str(output_path),
+    ]
+
+    cmd = [
+        arg for arg in cmd if arg
+    ]  # Filter out empty args, say in case of overwrite=False
+
+    try:
+        subprocess.run(cmd, capture_output=True, text=True, check=True)
+        print("File compressed successfully!")
+        return True
+
+    except subprocess.CalledProcessError as e:
+        print(f"FFmpeg error: {e.stderr}")
+        print("File compression failed.")
+        return False
